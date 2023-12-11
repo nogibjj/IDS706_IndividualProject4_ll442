baa2c7d0e118cf95c71ddafb04e8cca292488995
ll442 <ll442@duke.edu>
2023-12-11 11:36:46 -0500

    update readme

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def home():
 def about():
     return render_template('about.html', title='About Us')
 
+@app.route('/llm')
+def llm():
+    return render_template('llm.html', title='large language model function')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=1918)
